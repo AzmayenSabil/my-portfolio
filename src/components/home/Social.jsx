a254456f27f9751af965f1a758edff1e8f3948af
Azmayen Fayek Sabil <77103994+AzmayenSabil@users.noreply.github.com>
2023-01-01 01:16:44 +0600

    commit one 36-11
    problem with responsiveness

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+   <div className="home__social">
+    <a href="#insta" className="home__social-icon" target="_blank">
+        <i class="uil uil-instagram"></i>
+    </a>
+    <a href="#github" className="home__social-icon" target="_blank">
+        <i class="uil uil-github"></i>
+    </a>
+    <a href="#facebook" className="home__social-icon" target="_blank">
+        <i class="uil uil-facebook-f"></i>
+    </a>
+   </div>
+  )
+}
+
+export default Social
