157ac2af2215835b2c7790c64365884d56d6c0ce
Azmayen Fayek Sabil <77103994+AzmayenSabil@users.noreply.github.com>
2023-02-26 17:41:40 +0600

    2:04:35

--- src/components/qualification/Qualification.jsx
+++ src/components/qualification/Qualification.jsx
@@ -9,9 +9,142 @@ const Qualification = () => {
 
          <div className="qualification__container container">
             <div className="qualification__tabs">
-                <div className="qualification__button button--flex">
-                    
+                <div className="qualification__button qualification__active button--flex">
+                    <i className="uil uil-graduation-cap qualification__icon"></i>{" "} Education
                 </div>
+                <div className="qualification__button button--flex">
+                    <i className="uil uil-briefcase-alt qualification__icon"></i>{" "} Experience
+                </div>
+            </div>
+            <div className="qualification__sections">
+              <div className="qualification__content qualification__content-active">
+
+                <div className="qualification__data">
+                  <div>
+                    <h3 className="qualification__title">Web Developer</h3>
+                    <span className="qualification__subtitle">
+                      Spain - Institute
+                    </span>
+                    <div className="qualification__calender">
+                      <i className="uil uil-calendar-alt"></i> 2021 - present
+                    </div>
+                  </div>
+
+                  <div>
+                    <span className="qualifiaction__rounder"></span>
+                    <span className="qualification__line"></span>
+                  </div>
+                </div>
+
+                <div className="qualification__data">
+                  <div></div>
+                  <div>
+                    <span className="qualifiaction__rounder"></span>
+                    <span className="qualification__line"></span>
+                  </div>
+                  <div>
+                    <h3 className="qualification__title">Problem solver</h3>
+                    <span className="qualification__subtitle">
+                      Spain - Institute
+                    </span>
+                    <div className="qualification__calender">
+                      <i className="uil uil-calendar-alt"></i> 2021 - present
+                    </div>
+                  </div>
+
+                </div>
+
+                <div className="qualification__data">
+                  <div>
+                    <h3 className="qualification__title">Web Developer</h3>
+                    <span className="qualification__subtitle">
+                      Spain - Institute
+                    </span>
+                    <div className="qualification__calender">
+                      <i className="uil uil-calendar-alt"></i> 2021 - present
+                    </div>
+                  </div>
+
+                  <div>
+                    <span className="qualifiaction__rounder"></span>
+                    <span className="qualification__line"></span>
+                  </div>
+                </div>
+
+                <div className="qualification__data">
+                  <div></div>
+                  <div>
+                    <span className="qualifiaction__rounder"></span>
+                    <span className="qualification__line"></span>
+                  </div>
+                  <div>
+                    <h3 className="qualification__title">Problem solver</h3>
+                    <span className="qualification__subtitle">
+                      Spain - Institute
+                    </span>
+                    <div className="qualification__calender">
+                      <i className="uil uil-calendar-alt"></i> 2021 - present
+                    </div>
+                  </div>
+
+                </div>
+                
+              </div>
+
+              <div className="qualification__content">
+
+                <div className="qualification__data">
+                  <div>
+                    <h3 className="qualification__title">Web Developer</h3>
+                    <span className="qualification__subtitle">
+                      Spain - Institute
+                    </span>
+                    <div className="qualification__calender">
+                      <i className="uil uil-calendar-alt"></i> 2021 - present
+                    </div>
+                  </div>
+
+                  <div>
+                    <span className="qualifiaction__rounder"></span>
+                    <span className="qualification__line"></span>
+                  </div>
+                </div>
+
+                <div className="qualification__data">
+                  <div></div>
+                  <div>
+                    <span className="qualifiaction__rounder"></span>
+                    <span className="qualification__line"></span>
+                  </div>
+                  <div>
+                    <h3 className="qualification__title">Problem solver</h3>
+                    <span className="qualification__subtitle">
+                      Spain - Institute
+                    </span>
+                    <div className="qualification__calender">
+                      <i className="uil uil-calendar-alt"></i> 2021 - present
+                    </div>
+                  </div>
+
+                </div>
+
+                <div className="qualification__data">
+                  <div>
+                    <h3 className="qualification__title">Web Developer</h3>
+                    <span className="qualification__subtitle">
+                      Spain - Institute
+                    </span>
+                    <div className="qualification__calender">
+                      <i className="uil uil-calendar-alt"></i> 2021 - present
+                    </div>
+                  </div>
+
+                  <div>
+                    <span className="qualifiaction__rounder"></span>
+                    <span className="qualification__line"></span>
+                  </div>
+                </div>
+              </div>
             </div>
          </div>
     </section>
